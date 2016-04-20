603dcb019cbb2fbb18eb828052248b3036143dc6
Stefan Pavikevik <stefan_pavikevik@outlook.com>
2016-04-20 16:07:17 +0200

    Finished basic behavior of Product class

--- lib/product.rb
+++ lib/product.rb
@@ -1,24 +1,38 @@
 class Product
-  attr_reader :title
+  attr_reader :title, :price, :stock
 
   @@products = []
 
   def initialize(options = {})
     @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
     add_to_products
+  end
+
+  def in_stock?
+    stock > 0 ? true : false
   end
 
   def self.all
     @@products
   end
 
+  def self.in_stock
+    @@products.select {|prod| prod.in_stock?}
+  end
+
+  def self.find_by_title(title)
+    @@products.find {|prod| prod.title == title}
+  end
+
   private
 
   def add_to_products
-    # if @@products.index {|prod| prod.title == self.title}
-    #   raise DuplicateProductError, "#{self.title} already exists."
-    # else
+    if @@products.index {|prod| prod.title == self.title}
+      raise DuplicateProductError, "#{self.title} already exists."
+    else
       @@products << self
-    # end
+    end
   end
 end
