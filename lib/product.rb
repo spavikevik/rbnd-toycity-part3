5258f952bfe6d55e7d3efe0fca92e3341926ece3
Stefan Pavikevik <stefan_pavikevik@outlook.com>
2016-04-20 11:25:36 +0200

    Created DuplicateProductError and added add_to_products method that will create a new Product instance if it doesn't already exist in @@products. Otherwise, it will raise an error.

--- lib/product.rb
+++ lib/product.rb
@@ -1,10 +1,24 @@
 class Product
+  attr_reader :title
+
   @@products = []
+
   def initialize(options = {})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  private
+
+  def add_to_products
+    # if @@products.index {|prod| prod.title == self.title}
+    #   raise DuplicateProductError, "#{self.title} already exists."
+    # else
+      @@products << self
+    # end
   end
 end
